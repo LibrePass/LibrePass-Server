484be5cd8aba4d58848b36e507ca2aa2b8385f8c
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-01-30 19:39:00 +0100

    Add AuthController
    Added support for creating new accounts and logging into your account.

---
+++ server/src/main/kotlin/dev/medzik/vaultbox/server/controllers/api/v1/AuthController.kt
@@ -0,0 +1,33 @@
+package dev.medzik.vaultbox.server.controllers.api.v1
+
+import dev.medzik.vaultbox.server.services.UserService
+import dev.medzik.vaultbox.server.utils.Response
+import dev.medzik.vaultbox.server.utils.ResponseError
+import dev.medzik.vaultbox.server.utils.ResponseHandler
+import dev.medzik.vaultbox.types.api.auth.LoginRequest
+import dev.medzik.vaultbox.types.api.auth.RegisterRequest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/auth")
+class AuthController {
+    @Autowired
+    private lateinit var userService: UserService
+
+    @PostMapping("/register")
+    fun register(@RequestBody request: RegisterRequest): Response {
+        userService.register(request.email, request.password, request.passwordHint, request.encryptionKey)
+        return ResponseHandler.generateResponse(HttpStatus.CREATED)
+    }
+
+    @PostMapping("/login")
+    fun login(@RequestBody request: LoginRequest): Response {
+        val token = userService.login(request.email, request.password) ?: return ResponseError.InvalidCredentials
+        return ResponseHandler.generateResponse(token, HttpStatus.OK)
+    }
+}
