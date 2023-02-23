171a1f5b879d660041d245e56748a238b7033ba0
M3DZIK <me@medzik.dev>
2023-02-23 14:27:13 +0100

    Rebrand to LibrePass

---
+++ server/src/main/kotlin/dev/medzik/librepass/server/components/AuthorizedUser.kt
@@ -0,0 +1,38 @@
+package dev.medzik.librepass.server.components
+
+import dev.medzik.librepass.server.database.UserTable
+import dev.medzik.librepass.server.services.UserService
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.MethodParameter
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.ANNOTATION_CLASS)
+annotation class AuthorizedUser {}
+
+@Component
+class AuthorizedUserArgumentResolver : HandlerMethodArgumentResolver {
+    @Autowired
+    private lateinit var userService: UserService
+
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.hasParameterAnnotation(AuthorizedUser::class.java)
+    }
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?
+    ): UserTable? {
+        val request = webRequest.getNativeRequest(HttpServletRequest::class.java) ?: return null
+        val authorizationHeader = request.getHeader("Authorization") ?: return null
+        val token = authorizationHeader.removePrefix("Bearer ")
+        return userService.getUserByToken(token)
+    }
+}
--- server/src/main/kotlin/dev/medzik/vaultbox/server/database/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package dev.medzik.vaultbox.server.database
-
-import org.springframework.data.repository.CrudRepository
-import java.util.*
-
-interface UserRepository : CrudRepository<UserTable, UUID> {
-    fun findByEmail(email: String): UserTable?
-}
