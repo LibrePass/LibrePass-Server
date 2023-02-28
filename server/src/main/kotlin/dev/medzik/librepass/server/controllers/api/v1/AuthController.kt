39e0d7c30d96689b0c3179eabba9564d53674113
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-02-28 19:05:48 +0100

    Add rate limit on login request in AuthController

--- server/src/main/kotlin/dev/medzik/librepass/server/controllers/api/v1/AuthController.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/controllers/api/v1/AuthController.kt
@@ -7,18 +7,40 @@ import dev.medzik.librepass.server.utils.ResponseHandler
 import dev.medzik.librepass.types.api.auth.LoginRequest
 import dev.medzik.librepass.types.api.auth.RefreshRequest
 import dev.medzik.librepass.types.api.auth.RegisterRequest
+import io.github.bucket4j.Bandwidth
+import io.github.bucket4j.Bucket
+import io.github.bucket4j.Refill
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.Duration
+import java.util.concurrent.ConcurrentHashMap
+
+class AuthRateLimitConfig {
+    private var cache: Map<String, Bucket> = ConcurrentHashMap()
+
+    fun resolveBucket(key: String): Bucket {
+        return cache[key] ?: newBucket().also { cache += key to it }
+    }
+
+    fun newBucket(): Bucket {
+        return Bucket.builder()
+            .addLimit(Bandwidth.classic(20, Refill.intervally(10, Duration.ofMinutes(1))))
+            .build()
+    }
+}
 
 @RestController
 @RequestMapping("/api/v1/auth")
 class AuthController {
     @Autowired
     private lateinit var userService: UserService
+
+    private val rateLimit = AuthRateLimitConfig()
 
     @PostMapping("/register")
     fun register(@RequestBody request: RegisterRequest): Response {
@@ -30,8 +52,14 @@ class AuthController {
     }
 
     @PostMapping("/login")
-    fun login(@RequestBody request: LoginRequest): Response {
+    fun login(httpServletRequest: HttpServletRequest, @RequestBody request: LoginRequest): Response {
         val credentials = userService.login(request.email, request.password) ?: return ResponseError.InvalidCredentials
+
+        val ip = httpServletRequest.remoteAddr
+        if (!rateLimit.resolveBucket(ip).tryConsume(1)) {
+            return ResponseError.TooManyRequests
+        }
+
         return ResponseHandler.generateResponse(credentials, HttpStatus.OK)
     }
 
