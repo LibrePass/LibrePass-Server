ab09e3dff8a472efca8ce2719f7bb6d2348f8a11
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-03-02 22:37:29 +0100

    Send email with verification code
    Currently only added sending email. (required testing)
    
    TODO: implement verifing email in AuthController.

---
+++ server/src/main/kotlin/dev/medzik/librepass/server/services/EmailService.kt
@@ -0,0 +1,31 @@
+package dev.medzik.librepass.server.services
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.stereotype.Service
+
+@Service
+class EmailService {
+    @Autowired
+    private lateinit var emailSender: JavaMailSender
+    @Value("smtp.mail.address")
+    private lateinit var senderAddress: String
+
+    fun send(to: String, subject: String, body: String) {
+        val message = SimpleMailMessage()
+        message.from = senderAddress
+        message.setTo(to)
+        message.subject = subject
+        message.text = body
+
+        emailSender.send(message)
+    }
+
+    fun sendEmailVerification(to: String, code: String) {
+        val subject = "Activate your LibrePass account"
+        val body = "Click here to activate your account https://example.com/api/v1/auth/verifyEmail?code=$code"
+        send(to, subject, body)
+    }
+}
