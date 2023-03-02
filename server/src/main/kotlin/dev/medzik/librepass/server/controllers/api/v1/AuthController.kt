620dc62cad1da628b3608f5f4730dee6610fb0a7
M3DZIK <me@medzik.dev>
2023-03-02 22:37:29 +0100

    Send email with verification code
    Currently only added sending email. (required testing)
    
    TODO: implement verifing email in AuthController.

--- server/src/main/kotlin/dev/medzik/librepass/server/controllers/api/v1/AuthController.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/controllers/api/v1/AuthController.kt
@@ -1,5 +1,6 @@
 package dev.medzik.librepass.server.controllers.api.v1
 
+import dev.medzik.librepass.server.services.EmailService
 import dev.medzik.librepass.server.services.UserService
 import dev.medzik.librepass.server.utils.Response
 import dev.medzik.librepass.server.utils.ResponseError
@@ -13,6 +14,7 @@ import io.github.bucket4j.Refill
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -27,7 +29,7 @@ class AuthRateLimitConfig {
         return cache[key] ?: newBucket().also { cache += key to it }
     }
 
-    fun newBucket(): Bucket {
+    private fun newBucket(): Bucket {
         return Bucket.builder()
             .addLimit(Bandwidth.classic(20, Refill.intervally(10, Duration.ofMinutes(1))))
             .build()
@@ -39,14 +41,17 @@ class AuthRateLimitConfig {
 class AuthController {
     @Autowired
     private lateinit var userService: UserService
+    @Autowired
+    private lateinit var emailService: EmailService
 
     private val rateLimit = AuthRateLimitConfig()
 
     @PostMapping("/register")
     fun register(@RequestBody request: RegisterRequest): Response {
-        userService.register(request.email, request.password, request.passwordHint, request.encryptionKey)
+        val verificationToken = userService.register(request.email, request.password, request.passwordHint, request.encryptionKey)
 
-        // TODO: send email with verification link
+        // TODO: run in background
+        emailService.sendEmailVerification(request.email, verificationToken)
 
         return ResponseHandler.generateResponse(HttpStatus.CREATED)
     }
@@ -67,5 +72,10 @@ class AuthController {
     fun refresh(@RequestBody request: RefreshRequest): Response {
         val credentials = userService.refreshToken(request.refreshToken) ?: return ResponseError.InvalidCredentials
         return ResponseHandler.generateResponse(credentials, HttpStatus.OK)
+    }
+
+    @GetMapping("/verifyEmail")
+    fun verifyEmail(): Response {
+        return ResponseHandler.generateResponse(HttpStatus.NOT_IMPLEMENTED)
     }
 }
