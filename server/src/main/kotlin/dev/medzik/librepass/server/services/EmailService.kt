d15a39bd6206485ab4546c8a9b09b7b86084f56e
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-03-07 16:30:27 +0100

    Add smtp to github actions to fix tests

--- server/src/main/kotlin/dev/medzik/librepass/server/services/EmailService.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/services/EmailService.kt
@@ -10,12 +10,12 @@ import org.springframework.stereotype.Service
 class EmailService {
     @Autowired
     private lateinit var emailSender: JavaMailSender
-    @Value("smtp.mail.address")
-    private lateinit var senderAddress: String
+//    @Value("smtp.mail.address")
+//    private lateinit var senderAddress: String
 
     fun send(to: String, subject: String, body: String) {
         val message = SimpleMailMessage()
-        message.from = senderAddress
+//        message.from = senderAddress
         message.setTo(to)
         message.subject = subject
         message.text = body
