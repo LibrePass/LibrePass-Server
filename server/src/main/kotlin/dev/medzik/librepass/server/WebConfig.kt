21b1e64be366a2ce4cbdbd37cc9ee5cb1efaa381
M3DZIK <me@medzik.dev>
2023-03-12 14:04:49 +0100

    Add support for allow CORS origins

--- server/src/main/kotlin/dev/medzik/librepass/server/WebConfig.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/WebConfig.kt
@@ -2,16 +2,32 @@ package dev.medzik.librepass.server
 
 import dev.medzik.librepass.server.components.AuthorizedUserArgumentResolver
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
+    @Value("cors.allowedOrigins")
+    private lateinit var allowedOrigins: String
+
     @Autowired
     private lateinit var authorizedUserArgumentResolver: AuthorizedUserArgumentResolver
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(authorizedUserArgumentResolver)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        val origins = allowedOrigins.split(" ").toTypedArray()
+
+        registry.addMapping("/**")
+            .allowedOrigins(*origins)
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
     }
 }
