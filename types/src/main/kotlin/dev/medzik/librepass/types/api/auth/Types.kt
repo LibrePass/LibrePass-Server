060977f7490195ebfdebd9066e27a8f2564cbc93
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-04-17 23:01:53 +0200

    Use kotlin serialization for json processing

--- types/src/main/kotlin/dev/medzik/librepass/types/api/auth/Types.kt
+++ types/src/main/kotlin/dev/medzik/librepass/types/api/auth/Types.kt
@@ -1,10 +1,13 @@
 package dev.medzik.librepass.types.api.auth
 
+import dev.medzik.librepass.types.api.serializers.UUIDSerializer
+import kotlinx.serialization.Serializable
 import java.util.*
 
 /**
  * RegisterRequest is a representation of a request to register endpoint.
  */
+@Serializable
 data class RegisterRequest(
     val email: String,
     val password: String,
@@ -15,6 +18,7 @@ data class RegisterRequest(
 /**
  * LoginRequest is a representation of a request to login endpoint.
  */
+@Serializable
 data class LoginRequest(
     val email: String,
     val password: String
@@ -23,13 +27,16 @@ data class LoginRequest(
 /**
  * RefreshRequest is a representation of a request to refresh endpoint.
  */
+@Serializable
 data class RefreshRequest(val refreshToken: String)
 
 /**
  * UserCredentials is a representation of a user credentials.
  * Server returns this object after successful login or refresh.
  */
+@Serializable
 data class UserCredentials(
+    @Serializable(with = UUIDSerializer::class)
     val userId: UUID,
     val accessToken: String,
     val refreshToken: String,
