5c2e68836524a788598484a9764ddcb6f386f6c6
M3DZIK <me@medzik.dev>
2023-04-29 22:49:25 +0200

    server: Breaking changes in CipherController
    Now `GET /` returns full ciphers list (List<EncryptedCipher>) instead only IDs (List<UUID>)
    
    Added `GET /sync/{lastSync}` that returns SyncResponse
      ids - to compare  which ciphers have been removed,
      ciphers - which contains only new and modified ciphers
    
    Also implemented to `client`.

--- types/src/main/kotlin/dev/medzik/librepass/types/api/cipher/Types.kt
+++ types/src/main/kotlin/dev/medzik/librepass/types/api/cipher/Types.kt
@@ -1,11 +1,35 @@
 package dev.medzik.librepass.types.api.cipher
 
+import dev.medzik.librepass.types.api.EncryptedCipher
 import dev.medzik.librepass.types.api.serializers.UUIDSerializer
 import kotlinx.serialization.Serializable
 import java.util.*
 
+/**
+ * Response for insert request.
+ */
 @Serializable
 data class InsertResponse(
+    /**
+     * ID of the inserted cipher.
+     */
     @Serializable(with = UUIDSerializer::class)
     val id: UUID
+)
+
+/**
+ * Response for sync request.
+ */
+@Serializable
+data class SyncResponse(
+    /**
+     * List of IDs of all ciphers.
+     *
+     * Used for checking if cipher was deleted, it will not be in this list.
+     */
+    val ids: List<@Serializable(with = UUIDSerializer::class) UUID>,
+    /**
+     * List of all ciphers that were updated after timestamp.
+     */
+    val ciphers: List<EncryptedCipher>
 )
