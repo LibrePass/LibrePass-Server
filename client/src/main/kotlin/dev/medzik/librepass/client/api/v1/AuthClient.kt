ca3d49f99d9567a1cb488da786782a9f062bd952
M3DZIK <me@medzik.dev>
2023-05-01 22:53:47 +0200

    Use argon2id instead of pbkdf2

--- client/src/main/kotlin/dev/medzik/librepass/client/api/v1/AuthClient.kt
+++ client/src/main/kotlin/dev/medzik/librepass/client/api/v1/AuthClient.kt
@@ -1,6 +1,7 @@
 package dev.medzik.librepass.client.api.v1
 
 import dev.medzik.libcrypto.AesCbc
+import dev.medzik.libcrypto.Argon2id
 import dev.medzik.libcrypto.Pbkdf2
 import dev.medzik.libcrypto.Salt
 import dev.medzik.librepass.client.Client
@@ -13,8 +14,9 @@ import dev.medzik.librepass.types.api.auth.UserCredentials
 import kotlinx.serialization.json.Json
 import org.apache.commons.codec.binary.Hex
 
-const val EncryptionKeyIterations = 500 // 500 iterations
-const val PasswordIterations = 65000 // 65k iterations
+const val EncryptionKeyIterations = 500
+
+val Argon2idHasher = Argon2id(32, 1, 47104, 1)
 
 @Suppress("unused")
 class AuthClient(apiUrl: String = Client.DefaultApiUrl) {
@@ -30,13 +32,11 @@ class AuthClient(apiUrl: String = Client.DefaultApiUrl) {
      */
     @Throws(ClientException::class, ApiException::class)
     fun register(email: String, password: String, passwordHint: String? = null) {
-        // compute the PBKDF2 sha256 hash of the password with 65k iterations and with email as salt
         val basePassword = computeBasePasswordHash(password, email)
         // compute the final password, it is required since the earlier hash is used to encrypt the encryption key
         val finalPassword = computeFinalPasswordHash(basePassword, email)
 
         // create a random byte array with 16 bytes and encode it to hex string,
-        // then compute the PBKDF2 sha256 hash with 500 iterations and with random salt of 16 bytes length
         val encryptionKeyBase = Pbkdf2(EncryptionKeyIterations).sha256(Hex.encodeHexString(Salt.generate(16)), Salt.generate(16))
         val encryptionKey = AesCbc.encrypt(encryptionKeyBase, basePassword)
 
@@ -59,7 +59,7 @@ class AuthClient(apiUrl: String = Client.DefaultApiUrl) {
      */
     @Throws(ClientException::class, ApiException::class)
     fun login(email: String, password: String, passwordIsBaseHash: Boolean = false): UserCredentials {
-        val basePassword = if (passwordIsBaseHash) password else Pbkdf2(PasswordIterations).sha256(password, email.encodeToByteArray())
+        val basePassword = if (passwordIsBaseHash) password else computeBasePasswordHash(password, email)
         val finalPassword = computeFinalPasswordHash(basePassword, email)
 
         val request = LoginRequest(
@@ -93,7 +93,8 @@ class AuthClient(apiUrl: String = Client.DefaultApiUrl) {
          * @param email email of the user
          */
         fun computeBasePasswordHash(password: String, email: String): String {
-            return Pbkdf2(PasswordIterations).sha256(password, email.encodeToByteArray())
+            val hash = Argon2idHasher.hash(password, email.encodeToByteArray())
+            return Argon2id.toHexHash(hash)
         }
 
         /**
