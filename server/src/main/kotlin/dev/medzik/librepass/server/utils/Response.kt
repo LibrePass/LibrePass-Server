5fa1a1a14e0d7435f92112c8ffb6dd9e0fe39b0f
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-05-06 15:26:38 +0200

    server: Automatic serializable response using Kotlinx Serialization

--- server/src/main/kotlin/dev/medzik/librepass/server/utils/Response.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/utils/Response.kt
@@ -1,5 +1,8 @@
 package dev.medzik.librepass.server.utils
 
+import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.serializer
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
@@ -12,7 +15,20 @@ object ResponseHandler {
     }
 
     fun generateResponse(data: Any, status: HttpStatus): Response {
-        return createResponse(data, status)
+        return createResponse(
+            data = encodeToJson(data),
+            status = status
+        )
+    }
+
+    /**
+     * Encode data to JSON string using Kotlinx Serialization
+     */
+    @OptIn(InternalSerializationApi::class)
+    private inline fun <reified T : Any> encodeToJson(data: T) {
+        val serializer = T::class.serializer()
+        val jsonString = Json.encodeToString(serializer, data)
+        println(jsonString)
     }
 
     fun generateErrorResponse(error: String, status: HttpStatus): Response {
@@ -36,12 +52,12 @@ object ResponseError {
     private fun generateErrorResponse(error: String, status: HttpStatus): Response = ResponseHandler.generateErrorResponse(error, status)
 
     val InvalidBody = generateErrorResponse("invalid_body", HttpStatus.BAD_REQUEST)
-    val InvalidToken = generateErrorResponse("invalid_token", HttpStatus.UNAUTHORIZED)
+//    val InvalidToken = generateErrorResponse("invalid_token", HttpStatus.UNAUTHORIZED)
     val InvalidCredentials = generateErrorResponse("invalid_credentials", HttpStatus.UNAUTHORIZED)
 
     val Unauthorized = generateErrorResponse("unauthorized", HttpStatus.UNAUTHORIZED)
     val NotFound = generateErrorResponse("not_found", HttpStatus.NOT_FOUND)
-    val Conflict = generateErrorResponse("conflict", HttpStatus.CONFLICT)
+//    val Conflict = generateErrorResponse("conflict", HttpStatus.CONFLICT)
     val TooManyRequests = generateErrorResponse("too_many_requests", HttpStatus.TOO_MANY_REQUESTS)
 
     // database errors
