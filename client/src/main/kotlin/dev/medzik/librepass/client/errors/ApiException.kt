ad9abd92a250a3df478c27bb6b7f71b35f8c4a5c
M3DZIK <me@medzik.dev>
2023-05-13 22:00:49 +0200

    client: Add HttpError enum in ApiException

--- client/src/main/kotlin/dev/medzik/librepass/client/errors/ApiException.kt
+++ client/src/main/kotlin/dev/medzik/librepass/client/errors/ApiException.kt
@@ -1,15 +1,27 @@
 package dev.medzik.librepass.client.errors
 
+enum class HttpError {
+    BAD_REQUEST,
+    UNAUTHORIZED,
+    NOT_FOUND,
+    TOO_MANY_REQUESTS,
+    DUPLICATE,
+    SERVER_ERROR,
+    UNKNOWN
+}
+
 @Suppress("unused")
 class ApiException(
     val status: Number,
     val error: String
 ) : Exception() {
-    override val message: String
-        get() = "API error: $error (code $status)"
-
-    /**
-     * Check if the error is a 401 Unauthorized
-     */
-    fun isUnauthorized(): Boolean = status == 401
+    val httpError: HttpError = when (status.toInt()) {
+        400 -> HttpError.BAD_REQUEST
+        401 -> HttpError.UNAUTHORIZED
+        404 -> HttpError.NOT_FOUND
+        429 -> HttpError.TOO_MANY_REQUESTS
+        409 -> HttpError.DUPLICATE
+        500 -> HttpError.SERVER_ERROR
+        else -> HttpError.UNKNOWN
+    }
 }
