f2878898231e60c93ebf30058d843fdfcd33b20a
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-06-04 13:58:16 +0200

    Refactor code

--- types/src/main/kotlin/dev/medzik/librepass/types/api/ResponseError.kt
+++ types/src/main/kotlin/dev/medzik/librepass/types/api/ResponseError.kt
@@ -3,9 +3,7 @@ package dev.medzik.librepass.types.api
 import kotlinx.serialization.Serializable
 
 /**
- * Error response from the API. Contains the error message and the status code.
- * @property error The error message.
- * @property status The status code.
+ * Error response from the API.
  */
 @Serializable
 data class ResponseError(
