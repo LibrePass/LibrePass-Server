d1bda67e63fe8f93590bbf97c7d060d240421f7e
M3DZIK <me@medzik.dev>
2023-06-08 17:19:55 +0200

    Fix AuthorizedUser annonation

--- server/src/main/kotlin/dev/medzik/librepass/server/components/AuthorizedUser.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/components/AuthorizedUser.kt
@@ -49,7 +49,7 @@ class AuthorizedUserArgumentResolver @Autowired constructor(
         val tokenClaims = authComponent.parseToken(TokenType.API_KEY, token)
             ?: return null
         // get user id from token
-        val userID = tokenClaims[TokenClaims.TYPE.key].toString()
+        val userID = tokenClaims[TokenClaims.USER_ID.key] as String
 
         // get user from database
         val user = userRepository
