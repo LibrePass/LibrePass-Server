629c150952a84fab6663e37aa258acea22c238ae
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-06-22 16:27:22 +0200

    Refactor code

--- server/src/main/kotlin/dev/medzik/librepass/server/controllers/api/v1/UserController.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/controllers/api/v1/UserController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*
 import java.util.*
 
 /**
- * User controller. Handles user-related requests. Requires authentication.
+ * UserController handles user-related requests that require authentication.
  */
 @RestController
 @RequestMapping("/api/v1/user")
