0d41b3e8bb14acb8c0b2d988d3ee99ad107e38b9
M3DZIK <me@medzik.dev>
2023-06-30 23:04:34 +0200

    Fix tests

---
+++ client/src/test/kotlin/dev/medzik/librepass/client/api/UserClientTests.kt
@@ -0,0 +1,101 @@
+package dev.medzik.librepass.client.api
+
+import dev.medzik.librepass.types.cipher.Cipher
+import dev.medzik.librepass.types.cipher.CipherType
+import dev.medzik.librepass.types.cipher.EncryptedCipher
+import dev.medzik.librepass.types.cipher.data.CipherLoginData
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class UserClientTests {
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun setup() {
+            val authClient = AuthClient("http://localhost:8080")
+            authClient.register("test_user@example.com", "test")
+            // wait for 1 second to prevent unauthorized error
+            Thread.sleep(1000)
+        }
+    }
+
+    private lateinit var userId: UUID
+    private lateinit var secretKey: String
+
+    private lateinit var userClient: UserClient
+    private lateinit var cipherClient: CipherClient
+
+    @BeforeEach
+    fun beforeEach() {
+        val authClient = AuthClient("http://localhost:8080")
+        val credentials = authClient.login("test_user@example.com", "test")
+
+        userId = credentials.userId
+        secretKey = credentials.secretKey
+
+        userClient = UserClient("test_user@example.com", credentials.apiKey, "http://localhost:8080")
+        cipherClient = CipherClient(credentials.apiKey, "http://localhost:8080")
+    }
+
+    @Test
+    fun changePassword() {
+        val testCipher = Cipher(
+            id = UUID.randomUUID(),
+            owner = userId,
+            type = CipherType.Login,
+            loginData = CipherLoginData(
+                name = "test",
+                username = "test",
+                password = "test"
+            )
+        )
+
+        fun insertTestCipher() {
+            cipherClient.insert(EncryptedCipher(testCipher, secretKey))
+        }
+
+        fun checkCipher(secretKey: String) {
+            val ciphers = cipherClient.getAll()
+
+            val cipher = Cipher(ciphers[0], secretKey)
+
+            assertEquals(testCipher.loginData, cipher.loginData)
+        }
+
+        insertTestCipher()
+
+        checkCipher("test")
+
+        userClient.changePassword("test", "test2")
+
+        // wait for 1 second to prevent unauthorized error
+        Thread.sleep(1000)
+
+        // login with new password
+        var authClient = AuthClient("http://localhost:8080")
+        var credentials = authClient.login("test_user@example.com", "test2")
+        userClient = UserClient("test_user@example.com", credentials.apiKey, "http://localhost:8080")
+        cipherClient = CipherClient(credentials.apiKey, "http://localhost:8080")
+
+        checkCipher(credentials.secretKey)
+
+        // wait for 1 second to prevent unauthorized error
+        Thread.sleep(1000)
+
+        // change password back
+        userClient.changePassword("test2", "test")
+
+        // wait for 1 second to prevent unauthorized error
+        Thread.sleep(1000)
+
+        authClient = AuthClient("http://localhost:8080")
+        credentials = authClient.login("test_user@example.com", "test")
+        userClient = UserClient("test_user@example.com", credentials.apiKey, "http://localhost:8080")
+        cipherClient = CipherClient(credentials.apiKey, "http://localhost:8080")
+
+        checkCipher(credentials.secretKey)
+    }
+}
