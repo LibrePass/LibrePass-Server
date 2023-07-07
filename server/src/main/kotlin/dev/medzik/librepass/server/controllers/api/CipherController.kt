49c1e94c7e35218757f029f2fe159c85ee810912
M3DZIK <me@medzik.dev>
2023-07-07 15:38:06 +0200

    Update last modified date in cipher when updating in db

--- server/src/main/kotlin/dev/medzik/librepass/server/controllers/api/CipherController.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/controllers/api/CipherController.kt
@@ -99,7 +99,11 @@ class CipherController @Autowired constructor(
         if (!checkIfCipherExistsAndOwnedBy(id, user.id))
             return ResponseError.NOT_FOUND.toResponse()
 
-        cipherRepository.save(CipherTable(encryptedCipher))
+        cipherRepository.save(
+            CipherTable(
+                encryptedCipher.copy(lastModified = Date())
+            )
+        )
 
         return ResponseHandler.generateResponse(IdResponse(id), HttpStatus.OK)
     }
