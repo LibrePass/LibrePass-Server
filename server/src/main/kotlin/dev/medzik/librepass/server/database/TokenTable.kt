8a764280daf727eb78e3ca265ff9b2da57202940
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-07-07 21:31:25 +0200

    Fix auto-updating date in server database

--- server/src/main/kotlin/dev/medzik/librepass/server/database/TokenTable.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/database/TokenTable.kt
@@ -3,9 +3,12 @@ package dev.medzik.librepass.server.database
 import dev.medzik.libcrypto.Salt
 import jakarta.persistence.*
 import org.apache.commons.codec.binary.Hex
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.util.*
 
 @Entity
+@EntityListeners(AuditingEntityListener::class)
 @Table(name = "tokenTable")
 data class TokenTable(
     @Id
@@ -14,14 +17,13 @@ data class TokenTable(
 
     val lastIp: String,
 
+    @CreatedDate
     @Temporal(TemporalType.TIMESTAMP)
     val created: Date = Date(),
     @Temporal(TemporalType.TIMESTAMP)
     val lastUsed: Date = Date()
 ) {
     companion object {
-        fun generateToken(): String {
-            return "lp_" + Hex.encodeHexString(Salt.generate(32))
-        }
+        fun generateToken() = "lp_" + Hex.encodeHexString(Salt.generate(32))
     }
 }
