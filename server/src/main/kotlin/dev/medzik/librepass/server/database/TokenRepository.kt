b15cc36a81c6fa00f4d05645c16381cd82a12c0c
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-07-16 22:38:44 +0200

    Fix TokenRepository
    Signed-off-by: M3DZIK <me@medzik.dev>

--- server/src/main/kotlin/dev/medzik/librepass/server/database/TokenRepository.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/database/TokenRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository
 import java.util.*
 
 interface TokenRepository : CrudRepository<TokenTable, String> {
-    @Query("SELECT t FROM #{#entityName} t WHERE t.lastUsed < :createdBefore")
-    fun findAllByLastUsedBefore(lastUsed: Date): List<TokenTable>
+    @Query("SELECT t FROM #{#entityName} t WHERE t.lastUsed < :lastUsedBefore")
+    fun findAllByLastUsedBefore(lastUsedBefore: Date): List<TokenTable>
 }
