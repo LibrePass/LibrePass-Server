4a8499f9b51a0fa89e61d9f2203a700c4b4ee4ff
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-08-04 18:10:49 +0200

    Refactor rate limiter for AuthController

--- server/src/main/kotlin/dev/medzik/librepass/server/controllers/advice/CustomExceptions.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/controllers/advice/CustomExceptions.kt
@@ -1,23 +1,22 @@
 package dev.medzik.librepass.server.controllers.advice
 
 import dev.medzik.librepass.responses.ResponseError
-import dev.medzik.librepass.server.utils.Response
 import dev.medzik.librepass.server.utils.toResponse
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
 class AuthorizedUserException : Exception()
 class InvalidTwoFactorCodeException : Exception()
+class RateLimitException : Exception()
 
 @ControllerAdvice
 class CustomExceptions {
     @ExceptionHandler(value = [AuthorizedUserException::class])
-    fun authorizedUserException(): Response {
-        return ResponseError.UNAUTHORIZED.toResponse()
-    }
+    fun authorizedUserException() = ResponseError.UNAUTHORIZED.toResponse()
 
     @ExceptionHandler(value = [InvalidTwoFactorCodeException::class])
-    fun invalidTwoFactorCodeException(): Response {
-        return ResponseError.INVALID_CREDENTIALS.toResponse()
-    }
+    fun invalidTwoFactorCodeException() = ResponseError.INVALID_CREDENTIALS.toResponse()
+
+    @ExceptionHandler(value = [RateLimitException::class])
+    fun rateLimitException() = ResponseError.TOO_MANY_REQUESTS.toResponse()
 }
