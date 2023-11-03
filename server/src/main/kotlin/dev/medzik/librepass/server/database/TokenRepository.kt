9d0c5b56a825fc2499216b571236f797546f57f0
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-11-03 16:52:23 +0100

    server: Some code style changes

--- server/src/main/kotlin/dev/medzik/librepass/server/database/TokenRepository.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/database/TokenRepository.kt
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import java.util.*
 
+/** Repository for the [TokenTable]. */
 interface TokenRepository : CrudRepository<TokenTable, String> {
     @Query("SELECT t FROM #{#entityName} t WHERE t.lastUsed < :lastUsedBefore")
     fun findAllByLastUsedBefore(lastUsedBefore: Date): List<TokenTable>
