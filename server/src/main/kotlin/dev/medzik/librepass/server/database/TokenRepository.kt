8320d5d8e59be904af63144012b49e437a46a3b6
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-11-04 16:35:46 +0100

    server: Add schedule
    It removes unverified account and unused tokens.

--- server/src/main/kotlin/dev/medzik/librepass/server/database/TokenRepository.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/database/TokenRepository.kt
@@ -1,11 +1,16 @@
 package dev.medzik.librepass.server.database
 
+import jakarta.transaction.Transactional
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import java.util.*
 
 /** Repository for the [TokenTable]. */
 interface TokenRepository : CrudRepository<TokenTable, String> {
-    @Query("SELECT t FROM #{#entityName} t WHERE t.lastUsed < :lastUsedBefore")
-    fun findAllByLastUsedBefore(lastUsedBefore: Date): List<TokenTable>
+    /** Remove unused tokens that were last used before [lastUsedBefore]. */
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM #{#entityName} t WHERE t.lastUsed < :lastUsedBefore")
+    fun deleteUnused(lastUsedBefore: Date)
 }
