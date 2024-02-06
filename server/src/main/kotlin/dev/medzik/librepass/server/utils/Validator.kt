ffa64973f60e6a365b42e12175c4bcf7a1d6dd7e
M3DZIK <me@medzik.dev>
2024-02-06 21:14:02 +0100

    Add support for checking if the API URL is valid

--- server/src/main/kotlin/dev/medzik/librepass/server/utils/Validator.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/utils/Validator.kt
@@ -4,20 +4,12 @@ import dev.medzik.libcrypto.X25519
 import dev.medzik.librepass.server.controllers.api.ServerPrivateKey
 import dev.medzik.librepass.server.database.UserTable
 import dev.medzik.librepass.utils.fromHexString
-import org.apache.commons.validator.routines.EmailValidator
 import java.util.regex.Pattern
 
 object Validator {
     private val REGEX_PATTERN = Pattern.compile("^\\p{XDigit}+$")
 
-    fun emailValidator(email: String) = EmailValidator.getInstance().isValid(email)
-
     fun hexValidator(hex: String) = REGEX_PATTERN.matcher(hex).matches()
-
-    fun hexValidator(
-        hex: String,
-        length: Int
-    ) = REGEX_PATTERN.matcher(hex).matches() && hex.length == length * 2
 
     fun validateSharedKey(
         user: UserTable,
