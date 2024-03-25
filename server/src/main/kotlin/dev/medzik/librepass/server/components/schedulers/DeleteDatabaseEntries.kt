69ce213104758967f9fd7693cdf817476baa3bc4
M3DZIK <me@medzik.dev>
2024-03-25 22:07:10 +0100

    Use cron for scheduled tasks

--- server/src/main/kotlin/dev/medzik/librepass/server/components/schedulers/DeleteDatabaseEntries.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/components/schedulers/DeleteDatabaseEntries.kt
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.Instant
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 @Component
 class DeleteDatabaseEntries @Autowired constructor(
@@ -19,12 +20,12 @@ class DeleteDatabaseEntries @Autowired constructor(
     /**
      * Delete unverified users who have not verified their account for more than 24 hours.
      *
-     * This task is run once every 12 hours.
+     * This task is runs every 12 hours.
      */
-    @Scheduled(fixedRate = 12 * 60 * 60 * 1000)
+    @Scheduled(cron = "0 */12 * * * *")
     fun deleteUnverifiedAccountsTask() {
         if (emailVerificationRequired) {
-            val yesterdayDate = Instant.now().minusSeconds(24 * 60 * 60)
+            val yesterdayDate = Instant.now().minusSeconds(TimeUnit.DAYS.toSeconds(1))
             userRepository.deleteUnverifiedUsers(Date.from(yesterdayDate))
         }
     }
@@ -32,11 +33,11 @@ class DeleteDatabaseEntries @Autowired constructor(
     /**
      * Delete unused tokens that were not used for more than 30 days.
      *
-     * This task is run once every 12 hours.
+     * This task is run every 12 hours.
      */
-    @Scheduled(fixedRate = 12 * 60 * 60 * 1000)
+    @Scheduled(cron = "0 */12 * * * *")
     fun deleteUnusedTokensTask() {
-        val dateThirtyDaysAgo = Instant.now().minusSeconds(30 * 24 * 60 * 60)
+        val dateThirtyDaysAgo = Instant.now().minusSeconds(TimeUnit.DAYS.toSeconds(30))
         tokenRepository.deleteUnused(Date.from(dateThirtyDaysAgo))
     }
 }
