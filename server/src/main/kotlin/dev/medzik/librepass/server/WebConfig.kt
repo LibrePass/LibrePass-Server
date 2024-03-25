28e8052cac126ce0ef17014d3e87a794ba24a411
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-03-25 11:19:28 +0100

    Delete ktlint code formatter

--- server/src/main/kotlin/dev/medzik/librepass/server/WebConfig.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/WebConfig.kt
@@ -10,32 +10,30 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig
-    @Autowired
-    constructor(
-        // For @AuthorizedUser annotation
-        private val authorizedUserArgumentResolver: AuthorizedUserArgumentResolver,
-        // For @RequestIP annotation
-        private val requestIPArgumentResolver: RequestIPArgumentResolver
-    ) : WebMvcConfigurer {
-        override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
-            // Add @AuthorizedUser annotation support
-            resolvers.add(authorizedUserArgumentResolver)
-            // Add @RequestIP annotation support
-            resolvers.add(requestIPArgumentResolver)
-        }
-
-        // CORS configuration
-        @Value("\${server.cors.allowedOrigins}")
-        private lateinit var allowedOrigins: String
-
-        override fun addCorsMappings(registry: CorsRegistry) {
-            val allowedOrigin = allowedOrigins.split(",").toTypedArray()
-
-            registry.addMapping("/**")
-                .allowedOrigins(*allowedOrigin)
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-        }
+class WebConfig @Autowired constructor(
+    // For @AuthorizedUser annotation
+    private val authorizedUserArgumentResolver: AuthorizedUserArgumentResolver,
+    // For @RequestIP annotation
+    private val requestIPArgumentResolver: RequestIPArgumentResolver
+) : WebMvcConfigurer {
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        // Add @AuthorizedUser annotation support
+        resolvers.add(authorizedUserArgumentResolver)
+        // Add @RequestIP annotation support
+        resolvers.add(requestIPArgumentResolver)
     }
+
+    // CORS configuration
+    @Value("\${server.cors.allowedOrigins}")
+    private lateinit var allowedOrigins: String
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        val allowedOrigin = allowedOrigins.split(",").toTypedArray()
+
+        registry.addMapping("/**")
+            .allowedOrigins(*allowedOrigin)
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
--- server/src/main/kotlin/dev/medzik/librepass/server/controllers/api/Cipher.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/controllers/api/Cipher.kt
@@ -24,165 +24,163 @@ import java.util.concurrent.TimeUnit
 
 @RestController
 @RequestMapping("/api/cipher")
-class CipherController
-    @Autowired
-    constructor(
-        private val cipherRepository: CipherRepository,
-        @Value("\${server.api.rateLimit.enabled}")
-        private val rateLimitEnabled: Boolean,
-        @Value("\${limits.user.cipher}")
-        private val userCiphersLimit: Long
-    ) {
-        private val rateLimit = CipherControllerRateLimitConfig()
+class CipherController @Autowired constructor(
+    private val cipherRepository: CipherRepository,
+    @Value("\${server.api.rateLimit.enabled}")
+    private val rateLimitEnabled: Boolean,
+    @Value("\${limits.user.cipher}")
+    private val userCiphersLimit: Long
+) {
+    private val rateLimit = CipherControllerRateLimitConfig()
 
-        @PutMapping
-        fun saveCipher(
-            @AuthorizedUser user: UserTable,
-            @Valid @RequestBody encryptedCipher: EncryptedCipher
-        ): Response {
-            consumeRateLimit(user.id.toString())
+    @PutMapping
+    fun saveCipher(
+        @AuthorizedUser user: UserTable,
+        @Valid @RequestBody encryptedCipher: EncryptedCipher
+    ): Response {
+        consumeRateLimit(user.id.toString())
 
-            if (
-                // validates protected data
-                !Validator.hexValidator(encryptedCipher.protectedData) ||
-                // checks that the owner is correct
-                encryptedCipher.owner != user.id ||
-                // checks the user's cipher limit
-                (
-                    !cipherRepository.existsByIdAndOwner(encryptedCipher.id, user.id) &&
-                        cipherRepository.countByOwner(user.id) > userCiphersLimit
-                )
-            ) {
-                throw ServerException.InvalidCipher("validation failed")
-            }
-
-            val cipher = cipherRepository.save(CipherTable(encryptedCipher))
-
-            return ResponseHandler.generateResponse(
-                CipherIdResponse(cipher.id),
-                HttpStatus.CREATED
+        if (
+            // validates protected data
+            !Validator.hexValidator(encryptedCipher.protectedData) ||
+            // checks that the owner is correct
+            encryptedCipher.owner != user.id ||
+            // checks the user's cipher limit
+            (
+                !cipherRepository.existsByIdAndOwner(encryptedCipher.id, user.id) &&
+                    cipherRepository.countByOwner(user.id) > userCiphersLimit
             )
-        }
-
-        @GetMapping
-        fun getAllCiphers(
-            @AuthorizedUser user: UserTable
-        ): Response {
-            consumeRateLimit(user.id.toString())
-
-            val ciphers = cipherRepository.getAllByOwner(user.id)
-
-            // convert cipher table to encrypted ciphers
-            val response = ciphers.map { it.toEncryptedCipher() }
-
-            return ResponseHandler.generateResponse(response, HttpStatus.OK)
-        }
-
-        @GetMapping("/sync")
-        fun syncCiphers(
-            @AuthorizedUser user: UserTable,
-            @RequestParam("lastSync") lastSyncUnixTimestamp: Long
-        ): Response {
-            consumeRateLimit(user.id.toString())
-
-            val cipherIDs = cipherRepository.getAllIDs(user.id)
-
-            val ciphers =
-                cipherRepository.getAllByOwnerAndLastServerSync(
-                    user = user.id,
-                    date = Date(TimeUnit.SECONDS.toMillis(lastSyncUnixTimestamp))
-                )
-
-            val syncResponse =
-                SyncResponse(
-                    // get ids of all ciphers
-                    ids = cipherIDs,
-                    // get all ciphers that were updated after timestamp
-                    ciphers =
-                        ciphers
-                            // convert to encrypted ciphers
-                            .map { it.toEncryptedCipher() }
-                )
-
-            return ResponseHandler.generateResponse(syncResponse, HttpStatus.OK)
-        }
-
-        @GetMapping("/{id}")
-        fun getCipher(
-            @AuthorizedUser user: UserTable,
-            @PathVariable id: UUID
-        ): Response {
-            consumeRateLimit(user.id.toString())
-
-            if (!cipherRepository.existsByIdAndOwner(id, user.id))
-                throw ServerException.CipherNotFound()
-
-            val cipher = cipherRepository.findById(id).get()
-
-            // convert to encrypted cipher
-            val encryptedCipher = cipher.toEncryptedCipher()
-
-            return ResponseHandler.generateResponse(encryptedCipher, HttpStatus.OK)
-        }
-
-        @PatchMapping("/{id}")
-        @Deprecated("Use `/save` instead", ReplaceWith("saveCipher(user, encryptedCipher)"))
-        fun updateCipher(
-            @AuthorizedUser user: UserTable,
-            @PathVariable id: UUID,
-            @Valid @RequestBody encryptedCipher: EncryptedCipher
-        ): Response {
-            return saveCipher(user, encryptedCipher)
-        }
-
-        @DeleteMapping("/{id}")
-        fun deleteCipher(
-            @AuthorizedUser user: UserTable,
-            @PathVariable id: UUID
-        ): Response {
-            consumeRateLimit(user.id.toString())
-
-            if (!cipherRepository.existsByIdAndOwner(id, user.id))
-                throw ServerException.CipherNotFound()
-
-            cipherRepository.deleteById(id)
-
-            return ResponseHandler.generateResponse(CipherIdResponse(id), HttpStatus.OK)
-        }
-
-        @GetMapping("/icon")
-        fun getWebsiteIcon(
-            @RequestParam("domain") domain: String
-        ): Any {
-            // Some APIs to get website icon:
-            //  google api: https://www.google.com/s2/favicons?domain=$domain&sz=128
-            //  duckduckgo api: https://icons.duckduckgo.com/ip3/$domain.ico
-            //  icon.horse: https://icon.horse/icon/$domain
-
-            // get using google api
-            val uri = "https://www.google.com/s2/favicons?domain=$domain&sz=128"
-
-            val restTemplate = RestTemplate()
-
-            val headers = HttpHeaders()
-            headers.accept = listOf(MediaType.IMAGE_PNG)
-
-            val entity = HttpEntity<String>(headers)
-
-            return try {
-                restTemplate.exchange(uri, HttpMethod.GET, entity, ByteArray::class.java)
-            } catch (e: Exception) {
-                throw ServerException.NotFound()
-            }
-        }
-
-        private fun consumeRateLimit(
-            key: String,
-            rateLimitConfig: BaseRateLimitConfig = rateLimit
         ) {
-            if (!rateLimitEnabled) return
+            throw ServerException.InvalidCipher("validation failed")
+        }
 
-            if (!rateLimitConfig.resolveBucket(key).tryConsume(1))
-                throw ServerException.RateLimit()
+        val cipher = cipherRepository.save(CipherTable(encryptedCipher))
+
+        return ResponseHandler.generateResponse(
+            CipherIdResponse(cipher.id),
+            HttpStatus.CREATED
+        )
+    }
+
+    @GetMapping
+    fun getAllCiphers(
+        @AuthorizedUser user: UserTable
+    ): Response {
+        consumeRateLimit(user.id.toString())
+
+        val ciphers = cipherRepository.getAllByOwner(user.id)
+
+        // convert cipher table to encrypted ciphers
+        val response = ciphers.map { it.toEncryptedCipher() }
+
+        return ResponseHandler.generateResponse(response, HttpStatus.OK)
+    }
+
+    @GetMapping("/sync")
+    fun syncCiphers(
+        @AuthorizedUser user: UserTable,
+        @RequestParam("lastSync") lastSyncUnixTimestamp: Long
+    ): Response {
+        consumeRateLimit(user.id.toString())
+
+        val cipherIDs = cipherRepository.getAllIDs(user.id)
+
+        val ciphers =
+            cipherRepository.getAllByOwnerAndLastServerSync(
+                user = user.id,
+                date = Date(TimeUnit.SECONDS.toMillis(lastSyncUnixTimestamp))
+            )
+
+        val syncResponse =
+            SyncResponse(
+                // get ids of all ciphers
+                ids = cipherIDs,
+                // get all ciphers that were updated after timestamp
+                ciphers =
+                    ciphers
+                        // convert to encrypted ciphers
+                        .map { it.toEncryptedCipher() }
+            )
+
+        return ResponseHandler.generateResponse(syncResponse, HttpStatus.OK)
+    }
+
+    @GetMapping("/{id}")
+    fun getCipher(
+        @AuthorizedUser user: UserTable,
+        @PathVariable id: UUID
+    ): Response {
+        consumeRateLimit(user.id.toString())
+
+        if (!cipherRepository.existsByIdAndOwner(id, user.id))
+            throw ServerException.CipherNotFound()
+
+        val cipher = cipherRepository.findById(id).get()
+
+        // convert to encrypted cipher
+        val encryptedCipher = cipher.toEncryptedCipher()
+
+        return ResponseHandler.generateResponse(encryptedCipher, HttpStatus.OK)
+    }
+
+    @PatchMapping("/{id}")
+    @Deprecated("Use `/save` instead", ReplaceWith("saveCipher(user, encryptedCipher)"))
+    fun updateCipher(
+        @AuthorizedUser user: UserTable,
+        @PathVariable id: UUID,
+        @Valid @RequestBody encryptedCipher: EncryptedCipher
+    ): Response {
+        return saveCipher(user, encryptedCipher)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteCipher(
+        @AuthorizedUser user: UserTable,
+        @PathVariable id: UUID
+    ): Response {
+        consumeRateLimit(user.id.toString())
+
+        if (!cipherRepository.existsByIdAndOwner(id, user.id))
+            throw ServerException.CipherNotFound()
+
+        cipherRepository.deleteById(id)
+
+        return ResponseHandler.generateResponse(CipherIdResponse(id), HttpStatus.OK)
+    }
+
+    @GetMapping("/icon")
+    fun getWebsiteIcon(
+        @RequestParam("domain") domain: String
+    ): Any {
+        // Some APIs to get website icon:
+        //  google api: https://www.google.com/s2/favicons?domain=$domain&sz=128
+        //  duckduckgo api: https://icons.duckduckgo.com/ip3/$domain.ico
+        //  icon.horse: https://icon.horse/icon/$domain
+
+        // get using google api
+        val uri = "https://www.google.com/s2/favicons?domain=$domain&sz=128"
+
+        val restTemplate = RestTemplate()
+
+        val headers = HttpHeaders()
+        headers.accept = listOf(MediaType.IMAGE_PNG)
+
+        val entity = HttpEntity<String>(headers)
+
+        return try {
+            restTemplate.exchange(uri, HttpMethod.GET, entity, ByteArray::class.java)
+        } catch (e: Exception) {
+            throw ServerException.NotFound()
         }
     }
+
+    private fun consumeRateLimit(
+        key: String,
+        rateLimitConfig: BaseRateLimitConfig = rateLimit
+    ) {
+        if (!rateLimitEnabled) return
+
+        if (!rateLimitConfig.resolveBucket(key).tryConsume(1))
+            throw ServerException.RateLimit()
+    }
+}
