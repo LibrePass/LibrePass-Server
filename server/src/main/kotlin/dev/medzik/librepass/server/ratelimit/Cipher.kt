f266e995f319af52980c3c4889598073e3e0f360
M3DZIK <me@medzik.dev>
2024-03-27 22:13:40 +0100

    Refactor classes with rate limit configuration

--- server/src/main/kotlin/dev/medzik/librepass/server/ratelimit/Cipher.kt
+++ server/src/main/kotlin/dev/medzik/librepass/server/ratelimit/Cipher.kt
@@ -1,7 +1,5 @@
 package dev.medzik.librepass.server.ratelimit
 
-import io.github.bucket4j.BandwidthBuilder
-import io.github.bucket4j.Bucket
 import java.time.Duration
 
 /**
@@ -10,13 +8,7 @@ import java.time.Duration
  * Maximum of 200 tokens, refilled after one minute by 100 tokens.
  */
 class CipherControllerRateLimitConfig : BaseRateLimitConfig() {
-    override fun newBucket(): Bucket {
-        return Bucket.builder()
-            .addLimit {
-                    limit: BandwidthBuilder.BandwidthBuilderCapacityStage ->
-                limit.capacity(200)
-                    .refillGreedy(100, Duration.ofMinutes(1))
-            }
-            .build()
-    }
+    override val capacity: Long = 200
+    override val refill: Long = 100
+    override val refillDuration: Duration = Duration.ofMinutes(1)
 }
