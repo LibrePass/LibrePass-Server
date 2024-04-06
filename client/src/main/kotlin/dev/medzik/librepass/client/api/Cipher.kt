1dfb326466d6fffc92a26c525e12ab2e89ca2cea
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-04-06 21:24:55 +0200

    Add POST /sync endpoint to the CipherController
    Endpoint for synchronizing cipher with server (creating new ciphers, updating exists cipher, deleting ciphers and syncing ciphers from the server database with the local database).
    
    Deprecate all other endpoints in CipherController (save, get, delete) because the new sync endpoint can do it. Currently, it already exists for backward compatibility with older versions of applications.

--- client/src/main/kotlin/dev/medzik/librepass/client/api/Cipher.kt
+++ client/src/main/kotlin/dev/medzik/librepass/client/api/Cipher.kt
@@ -6,10 +6,12 @@ import dev.medzik.librepass.client.errors.ApiException
 import dev.medzik.librepass.client.errors.ClientException
 import dev.medzik.librepass.client.utils.JsonUtils
 import dev.medzik.librepass.types.api.CipherIdResponse
+import dev.medzik.librepass.types.api.SyncRequest
 import dev.medzik.librepass.types.api.SyncResponse
 import dev.medzik.librepass.types.cipher.Cipher
 import dev.medzik.librepass.types.cipher.EncryptedCipher
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**
  * Cipher Client for managing ciphers in the vault.
@@ -47,6 +49,7 @@ class CipherClient(
      * @param aesKey key for cipher encryption
      * @return [CipherIdResponse]
      */
+    @Deprecated("Use sync function")
     @Throws(ClientException::class, ApiException::class)
     fun save(
         cipher: Cipher,
@@ -66,6 +69,7 @@ class CipherClient(
      * @param cipher encrypted cipher to save
      * @return [CipherIdResponse]
      */
+    @Deprecated("Use sync function")
     @Throws(ClientException::class, ApiException::class)
     fun save(cipher: EncryptedCipher): CipherIdResponse {
         val response = client.put(API_ENDPOINT, JsonUtils.serialize(cipher))
@@ -78,6 +82,7 @@ class CipherClient(
      * @param id cipher identifier
      * @return [EncryptedCipher]
      */
+    @Deprecated("Use sync function")
     @Throws(ClientException::class, ApiException::class)
     fun get(id: UUID): EncryptedCipher {
         return get(id.toString())
@@ -89,6 +94,7 @@ class CipherClient(
      * @param id cipher identifier
      * @return [EncryptedCipher]
      */
+    @Deprecated("Use sync function")
     @Throws(ClientException::class, ApiException::class)
     fun get(id: String): EncryptedCipher {
         val response = client.get("$API_ENDPOINT/$id")
@@ -100,6 +106,7 @@ class CipherClient(
      *
      * @return list of [EncryptedCipher]
      */
+    @Deprecated("Use sync function")
     @Throws(ClientException::class, ApiException::class)
     fun getAll(): List<EncryptedCipher> {
         val response = client.get(API_ENDPOINT)
@@ -112,9 +119,28 @@ class CipherClient(
      * @param lastSync date of last successful synchronization
      * @return [SyncResponse]
      */
+    @Deprecated("Use new sync function with more fields")
     @Throws(ClientException::class, ApiException::class)
     fun sync(lastSync: Date): SyncResponse {
         val response = client.get("$API_ENDPOINT/sync?lastSync=${lastSync.time / 1000}")
+        return JsonUtils.deserialize(response)
+    }
+
+    /**
+     * Synchronizes the local database with the server database.
+     *
+     * @param lastSync date of the last synchronization
+     * @param updated new or updated ciphers to save into the server database
+     * @param deleted IDs with deleted ciphers to delete it from the server database
+     */
+    fun sync(lastSync: Date, updated: List<EncryptedCipher>, deleted: List<UUID>): SyncResponse {
+        val request = SyncRequest(
+            lastSyncTimestamp = TimeUnit.MILLISECONDS.toSeconds(lastSync.time),
+            updated,
+            deleted
+        )
+
+        val response = client.post("$API_ENDPOINT/sync", JsonUtils.serialize(request))
         return JsonUtils.deserialize(response)
     }
 
@@ -123,6 +149,7 @@ class CipherClient(
      *
      * @param id cipher identifier
      */
+    @Deprecated("Use sync function")
     @Throws(ClientException::class, ApiException::class)
     fun delete(id: UUID) {
         delete(id.toString())
@@ -133,6 +160,7 @@ class CipherClient(
      *
      * @param id cipher identifier
      */
+    @Deprecated("Use sync function")
     @Throws(ClientException::class, ApiException::class)
     fun delete(id: String) {
         client.delete("$API_ENDPOINT/$id")
