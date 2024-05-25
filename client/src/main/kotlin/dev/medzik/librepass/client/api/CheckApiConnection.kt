0da32fa615c276aec35124f3cff0095279289810
Oskar Karpiński <me@medzik.dev>
2024-05-25 15:20:35 +0200

    Fix `checkApiConnection` function
    Catch `JsonSyntaxException` due to not all invalid URLs returns json

--- client/src/main/kotlin/dev/medzik/librepass/client/api/CheckApiConnection.kt
+++ client/src/main/kotlin/dev/medzik/librepass/client/api/CheckApiConnection.kt
@@ -1,5 +1,6 @@
 package dev.medzik.librepass.client.api
 
+import com.google.gson.JsonSyntaxException
 import dev.medzik.librepass.client.Client
 import dev.medzik.librepass.client.errors.ApiException
 
@@ -7,7 +8,7 @@ import dev.medzik.librepass.client.errors.ApiException
  * Check connection with the API server.
  *
  * @param apiUrl server api url
- * @return whether connection has been established and if the server is LibrePass
+ * @return Whether connection has been established and if the server is LibrePass.
  */
 fun checkApiConnection(apiUrl: String): Boolean {
     val client = Client(apiUrl)
@@ -15,6 +16,8 @@ fun checkApiConnection(apiUrl: String): Boolean {
         val response = client.get("/actuator/info")
         return response.contains("\"group\":\"dev.medzik.librepass\"")
     } catch (e: ApiException) {
+        return false
+    } catch (e: JsonSyntaxException) {
         return false
     }
 }
