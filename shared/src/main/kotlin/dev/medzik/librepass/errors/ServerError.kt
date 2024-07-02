cf64d78f7f145515a4f2ab55f2e6f8a321c951fb
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-07-02 14:05:58 +0200

    Fix http status code for error `LP-User-404`

--- shared/src/main/kotlin/dev/medzik/librepass/errors/ServerError.kt
+++ shared/src/main/kotlin/dev/medzik/librepass/errors/ServerError.kt
@@ -18,7 +18,7 @@ enum class ServerError(val statusCode: HttpStatus, val error: String) {
     MissingCipher(HttpStatus.BAD_REQUEST, "LP-Missing-Cipher"),
     NotFound(HttpStatus.NOT_FOUND, "LP-404"),
     RateLimit(HttpStatus.TOO_MANY_REQUESTS, "LP-RateLimit"),
-    UserNotFound(HttpStatus.BAD_REQUEST, "LP-User-404"),
+    UserNotFound(HttpStatus.NOT_FOUND, "LP-User-404"),
     Database(HttpStatus.INTERNAL_SERVER_ERROR, "LP-Database-Error"),
     Mail(HttpStatus.INTERNAL_SERVER_ERROR, "LP-Mail-Error")
 }
